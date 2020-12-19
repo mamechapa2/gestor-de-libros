be56661c0aa1fcc58e7c0ccb9475f9075cb902c1
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2020-12-19 12:25:02 +0100

    main

--- src/ipo_practica_9/IPO_practica_9.java
+++ src/ipo_practica_9/IPO_practica_9.java
@@ -1,21 +1,79 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package ipo_practica_9;
 
-/**
- *
- * @author mamechapa
- */
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Vector;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/* ListDemo.java requires no other files. */
 public class IPO_practica_9 {
 
+    static Vector<Libro> librosEjemplo;
+
     /**
-     * @param args the command line arguments
+     * Create the GUI and show it. For thread safety, this method should be
+     * invoked from the event-dispatching thread.
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static void createAndShowGUI() {
+        librosEjemplo = new Vector<>();
+        librosEjemplo.add(new Libro("La chica de nieve", "Javier Castillo", "Thriller", 2020));
+        librosEjemplo.add(new Libro("La chica de nieve2", "Javier Castillo", "Thriller", 2020));
+        librosEjemplo.add(new Libro("La chica de nieve3", "Javier Castillo", "Thriller", 2020));
+        librosEjemplo.add(new Libro("La chica de nieve4", "Javier Castillo", "Thriller", 2020));
+        
+        //Create and set up the window.
+        JFrame frame = new JFrame("ListDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(500, 400));
+
+        //Create and set up the content pane.
+        JComponent newContentPane = new Lista(frame, librosEjemplo);
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
-    
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
