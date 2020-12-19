370e1c1c6c3ee2ab31dcc02f6622266e2a5d204a
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2020-12-19 16:25:49 +0100

    ventana de inicio con menubar y lista

--- src/ipo_practica_9/IPO_practica_9.java
+++ src/ipo_practica_9/IPO_practica_9.java
@@ -5,17 +5,57 @@
  */
 package ipo_practica_9;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
 /**
  *
  * @author mamechapa
  */
 public class IPO_practica_9 {
 
+    static private javax.swing.JMenu jMenu1;
+    static private javax.swing.JMenu jMenu2;
+    static private javax.swing.JMenuBar jMenuBar1;
+    static private javax.swing.JMenuItem jMenuItem2;
+    static private javax.swing.JMenuItem jMenuItem3;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                crearVentana();
+            }
+        });
     }
-    
+
+    private static void crearVentana() {
+        JFrame frame = new JFrame("App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(500, 400));
+        Inicio inicio = new Inicio(frame);
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu1.setText("Archivo");
+        jMenuItem3.setText("Idioma");
+        jMenu1.add(jMenuItem3);
+        jMenuItem2.setText("Salir");
+        jMenu1.add(jMenuItem2);
+        jMenuBar1.add(jMenu1);
+        jMenu2.setText("Ayuda");
+        jMenuBar1.add(jMenu2);
+        frame.setJMenuBar(jMenuBar1);
+        
+        frame.add(inicio);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
